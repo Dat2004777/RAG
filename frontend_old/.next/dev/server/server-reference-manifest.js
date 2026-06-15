c55357c0a5a9e7cac91a941fd86716c387f51805
Nguyễn DatJ <dat2004777@gmail.com>
2026-06-16 00:53:37 +0700

    refactor: package to old fe and be

---
+++ frontend_old/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wAVs6uzBjCwhoK+bFdswckz+qoMARZk8MLsUQlG99CQ=\"\n}"
---
+++ frontend_old/.next/dev/static/chunks/app_page_tsx_1-7dtlk._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/node_modules_0oq4d5i._.js","static/chunks/components_0xrttpc._.js"],
+    source: "dynamic"
+});
